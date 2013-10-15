61b9dd4b34e7b0e7dd40299e8772c5ed271832e0
Joze Perez <jozeperezp@gmail.com>
2013-10-15 10:27:07 -0400

    Made progress on api.

--- index.php
+++ index.php
@@ -1,11 +1,56 @@
 <?php
+/**
+ * JSONP Api
+ * --------
+ */
+class jsonApi {
+    private $apiForData;
 
-echo "<h1>HLF.com RESTfull API Index</h1>";
+    public function __construct( $data ) {
+        // save data for later use
+        $this->$apiForData = $data;
 
-if( count( $_POST ) > 0 ) {
-	echo "<ol>";
-	foreach( $_POST as $key => $val ) {
-		echo "<li>$key : $val</li>";
-	}
-	echo "</ol>";
+        // check for ANY data
+        if( count( $this->$apiForData ) > 0 ) {
+            die( "Missing ALL data." );
+        }
+
+        // check for ACTION data
+        if( !array_key_exists( 'action', $this->apiForData ) ) {
+            die( "Missing ACTION data" );
+        }
+
+        // check for METHOD data
+        if( !array_key_exists( 'method', $this->apiForData ) ) {
+            die( "Missing METHOD data" );
+        }
+
+        // send to router
+        $this->router( $this->apiForData['action'], $this->apiForData['method'] );
+    }
+
+    private function router( $action, $method ) {
+        switch( strtolower( $action ) ) {
+            case 'get':
+                # code...
+                break;
+            
+            case 'post':
+                # code...
+                break;
+
+            case 'put':
+                if( method_exists( 'jsonApi', 'putProductLead' ) ) {
+
+                }
+                break;
+
+            case 'delete':
+                # code...
+                break;
+        }
+    }
 }
+
+// initialie api
+$api = new jsonApi( $_GET );
